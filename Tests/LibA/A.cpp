2411ad25943cf46d7ff54c606b2d9a3daa5de900
Josh Petrie <josh@joshpetrie.net>
2026-05-03 09:49:12 -0700

    Add a basic test.

---
+++ Tests/LibA/A.cpp
@@ -0,0 +1,11 @@
+#include "A/A.hpp"
+
+#include <iostream>
+
+namespace a {
+  int test() {
+    std::cout << "This is library A.\n";
+    return 0;
+  }
+}
+
---
+++ Tests/T0-Basic/main.cpp
@@ -0,0 +1,5 @@
+#include <a/a.hpp>
+
+int main() {
+  return a::test();
+}
